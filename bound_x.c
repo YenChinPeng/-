cb73e9429088bce985120ee47f0a3e6eb2cb518d
YenChin <kugh30122@yahoo.com.tw>
2019-01-07 15:25:11 +0800

    devel test4-4

--- bound_x.c
+++ bound_x.c
@@ -5,6 +5,8 @@ struct A{
 int bound_ball_x(struct A **C,int ball_x,int ball_y)
 {
     printf("==================\n");
+    printf("the location of ball is:\t");
     printf("X:%d Y:%d\n",ball_x,ball_y);
     printf("==================\n");
+
 }
