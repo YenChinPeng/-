d2ae46b02e9f94850a0552552c8b32c9ec174e4b
YenChin <kugh30122@yahoo.com.tw>
2019-01-07 15:13:25 +0800

    test2

--- bound_x.c
+++ bound_x.c
@@ -1,1 +1,8 @@
-int bound_ball_x(struct A **C,ball_x,ball_y);
+struct A{
+  int photo;
+  int number;
+};
+int bound_ball_x(struct A **C,int ball_x,int ball_y);
+{
+    printf("X:%d Y:%d",ball_x,ball_y);
+}
